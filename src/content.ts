1c287d2215a81bd3ded827bdcff23e2fdbb2a7ea
Jamie Birch <14055146+shirakaba@users.noreply.github.com>
2021-06-25 00:20:03 +0100

    feat: Proof-of-concept for puck

--- src/content.ts
+++ src/content.ts
@@ -74,6 +74,7 @@ import {
   setPopupStyle,
 } from './popup';
 import { getPopupPosition, PopupPositionMode } from './popup-position';
+import { RikaiPuck } from './puck';
 import { query, QueryResult } from './query';
 import { isForeignObjectElement, isSvgDoc, isSvgSvgElement } from './svg';
 import { hasReasonableTimerResolution } from './timer-precision';
@@ -1135,6 +1136,19 @@ declare global {
   function onPageHide() {
     browser.runtime.sendMessage({ type: 'disabled' });
   }
+
+  window.addEventListener(
+    'DOMContentLoaded',
+    (event) => {
+      const rp = new RikaiPuck();
+      rp.render(document.body);
+      rp.enable();
+    },
+    {
+      once: true,
+    },
+  );
+
 })();
 
 export default ContentHandler;
